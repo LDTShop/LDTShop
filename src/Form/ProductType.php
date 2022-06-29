e0a6ac6873141ab8e5f983a7b1951b2334670d01
damvtn <damvtngcc200280@fpt.edu.vn>
2022-06-29 16:09:04 +0700

    Add functions create, update, show, and delete for Product entity

---
+++ src/Form/ProductType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('price')
+            ->add('description')
+            ->add('date')
+            ->add('quantity')
+            ->add('image')
+            ->add('status')
+            ->add('brand')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
