e0cdfb17f55a323b03814f7ebd5d906b3ff021e3
thulagcc <thulagcc200109@fpt.edu.vn>
2022-07-05 21:56:02 +0700

    Add function to change information account customer

--- src/Controller/CustomerController.php
+++ src/Controller/CustomerController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Customer;
+use App\Form\CustomerUserType;
 use App\Repository\CustomerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,6 +29,25 @@ class CustomerController extends AbstractController
         $customer = $this->getUser();
         return $this->render('customer/show.html.twig', [
             'customer'=>$customer
+        ]);
+    }
+    
+    /**
+     * @Route("/customer/account/{username}/edit", name="app_customer_account_edit", methods={"GET", "POST"})
+     */
+    public function FunctionName(Request $req, CustomerRepository $repo ,Customer $customer): Response
+    {
+        $form = $this->createForm(CustomerUserType::class, $customer);
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $repo->add($customer, true);
+
+            return $this->redirectToRoute('app_home_page', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('customer/edit.html.twig', [
+            'form' => $form,
         ]);
     }
 }
--- src/Form/CustomerUserType.php
+++ src/Form/CustomerUserType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Customer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,16 +16,20 @@ class CustomerUserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('roles')
-            ->add('password')
-            ->add('cusName')
-            ->add('gender')
-            ->add('address')
-            ->add('telephone')
-            ->add('email')
-            ->add('date')
-            ->add('status')
+        ->add('username')
+        ->add('cusName', TextType::class)
+        ->add('gender', ChoiceType::class,[
+            'choices'=>[
+                'Male'=>'0',
+                'Female'=>'1'
+            ],
+        ])
+        ->add('address', TextType::class)
+        ->add('telephone', TextType::class)
+        ->add('email', EmailType::class)
+        ->add('date', DateType::class,[
+            'widget'=>'single_text'
+        ])
         ;
     }
 
