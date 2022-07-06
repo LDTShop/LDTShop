7bafdc6afc31054517756c6e137d34c4718952f4
luan200292 <106128258+luan200292@users.noreply.github.com>
2022-07-06 23:01:59 +0700

    Add function delete product in cart

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -7,18 +7,38 @@ use App\Entity\Customer;
 use App\Entity\Product;
 use App\Repository\CartDetailRepository;
 use App\Repository\CartRepository;
+use App\Repository\OrdersDetailRepository;
 use App\Repository\ProductRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
 {
     /**
+     * @Route("/cart", name="app_show_cart")
+     */
+    public function showcartAction(CartDetailRepository $repo): Response
+    {
+        $user = $this->getUser();
+
+        if(!$user){
+            return $this->redirectToRoute('app_login');
+        }else{
+            $showcart = $repo->findAll();
+            return $this->render('cart/index.html.twig', [
+            'cart'=> $showcart
+            ]);
+        }
+    }
+    
+    /**
      * @Route("/cart/{id}", name="app_cart")
      */
-    public function index(CartRepository $repo, $id, ManagerRegistry $reg, ProductRepository $prorepo): Response
+    public function index(CartRepository $repo, $id, ManagerRegistry $reg, 
+    ProductRepository $prorepo, CartDetailRepository $cartDetailrepo): Response
     {
         $cartDetail = new CartDetail();
         $user = $this->getUser();
@@ -26,25 +46,41 @@ class CartController extends AbstractController
         $pro = $prorepo->find($id);
         $cart = $repo->findOneBy(['username' => $user]);
 
-        $cartDetail->setCart($cart);
-        $cartDetail->setProduct($pro);
-        $cartDetail->setQuantity(1);
-
         $entity = $reg->getManager();
 
-        $entity->persist($cartDetail);
+        $cartd = $cartDetailrepo->checkQuantity($id, $cart);
+
+        if($cartd[0]['count'] == 0){
+            $cartDetail->setCart($cart);
+            $cartDetail->setProduct($pro);
+            $cartDetail->setQuantity(1);
+    
+            $entity->persist($cartDetail);
+            $entity->flush();
+        }else{
+            $quantity = $cartd[0]['quantity'] + 1;
+            $cartdId = $cartd[0]['id'];
+
+            $cartDetail = $cartDetailrepo->find($cartdId);
+            $cartDetail->setQuantity($quantity);
+
+            $entity->persist($cartDetail);
+            $entity->flush();
+        }
+        return $this->redirectToRoute('app_show_cart');
+    }
+
+    /**
+     * @Route("/cart/delete/{id}", name="app_cart_delete")
+     */
+    public function delete($id, CartDetailRepository $repo, ManagerRegistry $reg): Response
+    {
+        $entity = $reg->getManager();
+        $cartd = $repo->find($id);
+
+        $entity->remove($cartd);
         $entity->flush();
 
         return $this->redirectToRoute('app_show_cart');
-    }
-    /**
-     * @Route("/cart", name="app_show_cart")
-     */
-    public function showcartAction(CartDetailRepository $repo): Response
-    {
-        $showcart = $repo->findAll();
-        return $this->render('cart/index.html.twig', [
-            'cart'=> $showcart
-        ]);
     }
 }
